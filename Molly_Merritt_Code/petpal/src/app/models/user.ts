2796f1e5755007f0e0df010f9764b1bdd3ad6b8c
mollymerritt <merritt.mry@gmail.com>
2018-09-08 18:40:06 -0400

    added user model

---
+++ Molly_Merritt_Code/petpal/src/app/models/user.ts
@@ -0,0 +1,11 @@
+export class User {
+    userId: number;
+    username: string;
+    password: string;
+    firstName: string;
+    lastName: string;
+    email: string;
+    dataRegistered: string; // time of registration
+    bio: string;
+    // ADD PET
+}
