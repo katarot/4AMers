2401caf7136371af8a38752d858a69f50169c967
mollymerritt <merritt.mry@gmail.com>
2018-09-08 02:07:11 -0400

    add comments

--- Molly_Merritt_Code/petpal/src/app/app.module.ts
+++ Molly_Merritt_Code/petpal/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
