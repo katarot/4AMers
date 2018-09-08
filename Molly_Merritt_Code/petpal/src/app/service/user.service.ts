af344ed40271843036015dd46e20242ecc3f221c
mollymerritt <merritt.mry@gmail.com>
2018-09-08 19:02:23 -0400

    add auth service

--- Molly_Merritt_Code/petpal/src/app/service/user.service.ts
+++ Molly_Merritt_Code/petpal/src/app/service/user.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { environment } from '../../environments/environment';
-import { User } from '../models/User';
+import { User } from '../model/User';
 
 @Injectable({
   providedIn: 'root'
