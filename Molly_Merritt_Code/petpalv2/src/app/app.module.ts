68a71b72c5562126f496016053a830aef2326d2e
mollymerritt <merritt.mry@gmail.com>
2018-09-10 22:56:21 -0400

    added components

--- Molly_Merritt_Code/petpalv2/src/app/app.module.ts
+++ Molly_Merritt_Code/petpalv2/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { AdminComponent } from './components/admin/admin.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    AdminComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
