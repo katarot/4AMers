2c2c3d4e5c44084a4e56afb34ca92e5edcfc478f
Omar Acevedo <omaracevedo@Omars-MacBook-Pro.local>
2018-09-11 16:19:38 -0400

    Angular version 1: shown today on presentation

---
+++ front-end/petsitterapp-angular/src/app/services/pet-crud.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { Pet } from '../models/pet.model';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PetCrudService {
+
+  constructor(private http: HttpClient) { }
+
+  public getPets() {
+    // return this.http.get<Pet[]>("http://localhost:8080/petsitterapp/pets");
+    return this.http.get<Pet[]>("http://18.232.118.152:8080/project2/pets");
+  }
+  
+}
