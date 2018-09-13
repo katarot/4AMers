34290ccfdbe3f45eaea6fe5b6d5dd85be9859c7c
mollymerritt <merritt.mry@gmail.com>
2018-09-13 03:01:00 -0400

    adding

---
+++ front-end/petsitterapp-angular/src/app/components/profile/addpetprofile/addpetprofile.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-addpetprofile',
+  templateUrl: './addpetprofile.component.html',
+  styleUrls: ['./addpetprofile.component.css']
+})
+export class AddpetprofileComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
