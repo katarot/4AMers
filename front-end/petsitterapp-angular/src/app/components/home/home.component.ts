dad10620ef6268f0135c1aa055b102a5eb34b07a
mollymerritt <merritt.mry@gmail.com>
2018-09-13 21:17:32 -0400

    small change

--- front-end/petsitterapp-angular/src/app/components/home/home.component.ts
+++ front-end/petsitterapp-angular/src/app/components/home/home.component.ts
@@ -12,6 +12,8 @@ import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 })
 export class HomeComponent implements OnInit {
 
+  closeResult: string;
+
   private username: string;
   private password: string;
   private user: User;
@@ -53,7 +55,21 @@ export class HomeComponent implements OnInit {
   }
 
   open(content) {
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
 
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return  `with: ${reason}`;
+    }
   }
 
 }
