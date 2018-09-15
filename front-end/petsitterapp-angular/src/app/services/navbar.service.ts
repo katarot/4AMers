4e4cab347f1a4fe05230aa8cca109a6c8327430c
mollymerritt <merritt.mry@gmail.com>
2018-09-15 11:12:02 -0400

    jk now added navbar service

---
+++ front-end/petsitterapp-angular/src/app/services/navbar.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NavbarService {
+
+  constructor(private cookieService: CookieService) { }
+
+  
+}
