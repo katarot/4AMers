85eee79376e7ab7c32343f46dd5930b7bc85b125
mollymerritt <merritt.mry@gmail.com>
2018-09-17 21:18:45 -0400

    populated textarea for updating bioDescription

--- front-end/petsitterapp-angular/src/app/components/profile/editprofile/editprofile.component.ts
+++ front-end/petsitterapp-angular/src/app/components/profile/editprofile/editprofile.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input, Output } from '@angular/core';
 import { ProfileComponent } from '../../profile/profile.component';
 import { EventEmitter } from '@angular/core';
 import { UploadFileService } from '../../../services/upload-file.service';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-editprofile',
@@ -11,18 +12,25 @@ import { UploadFileService } from '../../../services/upload-file.service';
 export class EditprofileComponent implements OnInit {
   @Output() bioUpdate = new EventEmitter<string>();
 
-  constructor(private upLoadService: UploadFileService) { }
+  constructor(private upLoadService: UploadFileService, private cookieService: CookieService) { }
   bioDescription: string;
   selectedFiles: FileList;
   imageSrc: any;
+  picSelected = false;
 
   updateBio() {
+    console.log(this.selectedFiles);
     this.bioUpdate.emit(this.bioDescription);
-    this.upload;
+    // if (this.picSelected) {
+      this.upload(); // this.upload;
+    // }
     this.bioUpdate.emit(this.imageSrc);
     console.log('inupdatebio');
+    // location.reload();
   }
   ngOnInit() {
+    this.bioDescription = JSON.parse(this.cookieService.get('user')).bioDescription;
+    console.log(this.bioDescription);
   }
 
   upload() {
@@ -31,7 +39,8 @@ export class EditprofileComponent implements OnInit {
     console.log(this.imageSrc);
   }
 
-   selectFile(event){
+   selectFile(event) {
+    this.picSelected = true;
     this.selectedFiles = event.target.files;
     console.log(this.selectedFiles[0].name);
   }
